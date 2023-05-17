cec04b47e44dcc6b83c94a1f253c9f676b008e05
MariamSulikashvili1 <116154200+MariamSulikashvili1@users.noreply.github.com>
2023-05-17 10:41:55 +0400

    Add files via upload

---
+++ Python-django-shell-Book application/Python-django-application/cache/OPTIONS.py
@@ -0,0 +1,5 @@
+"OPTIONS": {
+    "allow_unicode_keys": True,
+    "default_noreply": False,
+    "serde": pymemcache.serde.pickle_serde,
+}
---
+++ Python-django-shell-Book application/Python-django-application/cache/cache.py
@@ -0,0 +1,6 @@
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.memcached.PyMemcacheCache",
+        "LOCATION": "127.0.0.1:11211",
+    }
+}
